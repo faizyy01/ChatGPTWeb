bd4124202ce1a62083a58ec5665c6e2dc7c7d12b
Sleepingpirates <faizsyed.a@gmail.com>
2023-03-18 12:52:10 -0400

    !feat: auth warp the entire app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,43 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import { useSession, SessionProvider } from "next-auth/react";
+import { type AppProps } from "next/app";
+import { type ReactNode } from "react";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
+interface MyAppProps extends AppProps {
+  session: Session | null;
+}
+
+const MyApp: AppType<MyAppProps> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Auth>
+        <Component {...pageProps} />
+      </Auth>
     </SessionProvider>
   );
+};
+
+interface AuthProps {
+  children: ReactNode;
+}
+
+const Auth: React.FC<AuthProps> = ({ children }) => {
+  // if `{ required: true }` is supplied, `status` can only be "loading" or "authenticated"
+  const { status } = useSession({ required: true });
+
+  if (status === "loading") {
+    return <></>;
+  }
+
+  return <>{children}</>;
 };
 
 export default api.withTRPC(MyApp);
