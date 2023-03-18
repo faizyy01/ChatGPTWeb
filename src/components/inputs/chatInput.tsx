136f75e0c0435339afd0e69dec7ac638d59ee2dd
Sleepingpirates <faizsyed.a@gmail.com>
2023-03-18 12:51:30 -0400

    !feat: inital chatinput component

---
+++ src/components/inputs/chatInput.tsx
@@ -0,0 +1,36 @@
+import React, { useState, type FormEvent } from "react";
+import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
+
+interface ChatInputProps {
+  onSubmit: (message: string) => void;
+}
+
+const ChatInput: React.FC<ChatInputProps> = ({ onSubmit }) => {
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    void onSubmit(message);
+    setMessage("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="mt-4 flex">
+      <input
+        type="text"
+        className="w-full border border-gray-200 bg-black px-6 py-3 text-gray-300 focus:border-green-500 focus:outline-none"
+        placeholder="Type your message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="ml-3 border border-gray-200 bg-black px-6 py-3 font-semibold text-white hover:bg-white hover:text-black focus:outline-none"
+      >
+        <PaperAirplaneIcon className="h-6 w-6" />
+      </button>
+    </form>
+  );
+};
+
+export default ChatInput;
