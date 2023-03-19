99b4c86cc4b5dc2864e8c825d4b11b719376b4c6
Sleepingpirates <faizsyed.a@gmail.com>
2023-03-19 12:42:48 -0400

    ♻️ refactor(Sidebar): change onChatChange type signature to accept null values

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,17 +5,20 @@ import { type chat } from "@prisma/client";
 
 interface SidebarProps {
   chats: chat[];
-  onChatChange: (chat: chat) => void;
+  onChatChange: (chat: chat | null) => void;
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ chats, onChatChange }) => {
   return (
     <div className="relative hidden h-full w-64 border-r border-gray-700 md:block">
       <div className="px-4">
-        <h2 className="border border-gray-600 py-3 pl-3 text-left font-semibold text-gray-300">
+        <button
+          className="w-full border border-gray-600 py-3 px-6 text-left font-semibold text-gray-300"
+          onClick={() => void onChatChange(null)}
+        >
           <PlusIcon className="mr-1 inline-block h-6 w-6 text-gray-300/90" />
           New Chat
-        </h2>
+        </button>
         <div className="overflow-y-auto pt-4">
           <ul className="space-y-2">
             {chats.map((chat) => (
